28aa003234a8e15ae2fdec799e6a859890f48f83
Gabriel <knuckles4x@gmail.com>
2020-08-08 18:11:06 -0300

    TeacherList: connect to api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -8,36 +8,43 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.png";
 
 import styles from "./styles";
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image
           style={styles.avatar}
           source={{
-            uri: "https://github.com/gab618.png",
+            uri: teacher.avatar,
           }}
         />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Grabiel</Text>
-          <Text style={styles.subject}>Matemática</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
 
-      <Text style={styles.bio}>
-        Entusiasta das melhores tecnologias de química avançada.
-        {"\n"}
-        {"\n"}
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.00 pessoas já passaram por
-        um das minhas explosões. Inimigo número 1 da #PanelaCraft.
-      </Text>
+      <Text style={styles.bio}>{teacher.bio}</Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {"   "}
-          <Text style={styles.priceValue}> R$ 20,00</Text>
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+import api from "../../services/api";
 
 import styles from "./styles";
 
@@ -9,7 +10,6 @@ import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
-import api from "../../services/api";
 
 const Landing: React.FC = () => {
   const navigation = useNavigation();
